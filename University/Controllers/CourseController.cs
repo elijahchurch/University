527cf336a2303518d92bdbbfac56da3b7d5ae316
workmanmcr <workmanmcr@gmail.com>
2023-08-01 16:17:42 -0700

    add style and logo

--- University/Controllers/CourseController.cs
+++ University/Controllers/CourseController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using University.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace University.Controllers
 {
@@ -30,6 +31,57 @@ namespace University.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+          public ActionResult Details(int id)
+        {
+            Course thisCourse = _db.Courses
+                    .Include(course => course.JoinEntities)
+                    .ThenInclude(join => join.Student)
+                    .FirstOrDefault(course => course.CourseId == id);
+            return View(thisCourse);
+        }
         
+    public ActionResult AddStudent(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      ViewBag.StudentId = new SelectList(_db.Students, "StudentId", "Name");
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult AddStudent(Course course, int studentId)
+    {
+      #nullable enable
+      StudentCourse? joinEntity = _db.StudentCourses.FirstOrDefault(join => (join.CourseId == course.CourseId && join.StudentId == studentId));
+      #nullable disable
+      if (joinEntity == null && studentId != 0)
+      {
+        _db.StudentCourses.Add(new StudentCourse() { StudentId = studentId, CourseId = course.CourseId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = course.CourseId });
+    } 
+    public ActionResult Delete(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+        StudentCourse joinEntry = _db.StudentCourses.FirstOrDefault(entry => entry.StudentCourseID == joinId);
+        _db.StudentCourses.Remove(joinEntry);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
     }
 }
--- University/Controllers/StudentContoller.cs
+++ University/Controllers/StudentContoller.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using University.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace University.Controllers
 {
@@ -30,6 +31,36 @@ namespace University.Controllers
             _db.Students.Add(student);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Student thisStudent = _db.Students
+                    .Include(student => student.JoinEntities)
+                    .ThenInclude(join => join.Course)
+                    .FirstOrDefault(student => student.StudentId == id);
+            return View(thisStudent);
+        }
+
+        public ActionResult AddCourse(int id)
+        {
+        Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+        ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
+        return View(thisStudent);
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Student student, int courseId)
+        {
+            #nullable enable 
+            StudentCourse? joinEntity = _db.StudentCourses.FirstOrDefault(join => (join.StudentId == student.StudentId && join.CourseId == courseId));
+            #nullable disable
+            if (joinEntity == null && courseId != 0)
+            {
+                _db.StudentCourses.Add( new StudentCourse() {StudentId = student.StudentId, CourseId = courseId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new {id = student.StudentId});
         }
     }
 }
